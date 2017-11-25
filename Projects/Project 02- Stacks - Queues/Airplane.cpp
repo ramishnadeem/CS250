f4319678844740160e15b390136133140d22dbd8
Ramish Nadeem <nadeemramish98@gmail.com>
2017-11-25 17:42:05 -0600

    Add files via upload

---
+++ Projects/Project 02- Stacks - Queues/Airplane.cpp
@@ -0,0 +1,47 @@
+#include "Airplane.hpp"
+
+
+//! Move traveller onto airplane
+/**
+    For the traveller passed in, push it into the airplane's stack.
+
+    @param <Traveller*> traveller
+    @return <void>
+*/
+void Airplane::Board(Traveller* traveller)
+{
+	Stack_pass.Push(traveller);
+}
+
+//! Pull traveller off airplane
+/**
+    Get the next (top-most) traveller from the airplane stack,
+    remove them from the stack and return the traveller.
+
+    @return <Traveller*>
+*/
+Traveller* Airplane::Disembark()
+{
+	Traveller* stackTop = Stack_pass.Top();
+	Stack_pass.Pop();
+	return stackTop;
+}
+
+//! Return whether the airplane is empty (no passengers)
+/**
+    If the size of the stack is 0, then the airplane is empty.
+
+    @return <bool>
+*/
+bool Airplane::IsEmpty()
+{
+	if (Stack_pass.Size() == 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
---
+++ Projects/Project 02- Stacks - Queues/Airplane.hpp
@@ -0,0 +1,29 @@
+#ifndef _AIRPORT_HPP
+#define _AIRPORT_HPP
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <stack>
+#include <queue>
+#include <iomanip>
+using namespace std;
+
+#include "Traveller.hpp"
+
+#include "DataStructures/Stack.hpp"
+
+//! Airplane structure that has Board and Disembark functionality
+class Airplane
+{
+public:
+	void Board(Traveller* traveller);
+	Traveller* Disembark();
+	bool IsEmpty();
+
+private:
+	Stack<Traveller*>Stack_pass;
+};
+
+#endif
